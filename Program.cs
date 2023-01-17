d861e083c43ea9d1d0357fb5f6db628dbed472d3
Angelo Brocca <84419409+AngeloBrocca@users.noreply.github.com>
2023-01-17 14:59:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Insira numero para calculo");
+        string n = Console.ReadLine();
+        Console.WriteLine("______________________________________________");
+
+        int x = Convert.ToInt32(n);
+
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(x+" X "+i+" = "+i*x);
+        }
+
+    }
+}
